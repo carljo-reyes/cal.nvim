e723a29d5ce79e9a38a3965523c03a37d4f1540f
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-26 10:18:45 +0800

    cleanup

--- lua/cal/plugins/snacks-picker.lua
+++ lua/cal/plugins/snacks-picker.lua
@@ -1,24 +1,30 @@
 return {
-  "folke/snacks.nvim",
-  opts = function(_, opts)
-    opts.picker = opts.picker or {}
-    opts.picker.sources = opts.picker.sources or {}
-
-    opts.picker.sources.filetypes = require("cal.picker.filetypes")
-  end,
-  keys = {
-    {
-      "<Leader>ct",
-      function()
-        Snacks.picker.pick("filetypes")
-      end,
-      desc = "Change filetype",
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          filetypes = require("cal.picker.filetypes"),
+        },
+      },
     },
-    {
-      "<Space><Space>",
-      function()
-        Snacks.picker()
-      end,
+  },
+  {
+    "folke/snacks.nvim",
+    keys = {
+      {
+        "<Leader>ct",
+        function()
+          Snacks.picker.pick("filetypes")
+        end,
+        desc = "Change filetype",
+      },
+      {
+        "<Space><Space>",
+        function()
+          Snacks.picker()
+        end,
+      },
     },
   },
 }
--- lua/cal/plugins/theme.lua
+++ lua/cal/plugins/theme.lua
@@ -1,11 +1,4 @@
 return {
   "tribela/transparent.nvim",
   event = "VimEnter",
-  opts = {
-    auto = true,
-    extra_groups = {
-      "SnacksPickerNormal",
-      "SnacksPickerBorder",
-    },
-  },
 }
