4fd1e6c7e9ac633ecb6ad80241a34d03cccf2e4f
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-28 17:41:41 +0800

    remove labels on flash

---
+++ lua/cal/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/flash.nvim",
+  optional = true,
+  opts = {
+    modes = {
+      treesitter = {
+        label = {
+          before = false,
+          after = false,
+        },
+      },
+    },
+  },
+}
