e0da1ad926e1474648d80312987818d7082132db
Carljo Rommel Reyes <work.calreyes@gmail.com>
2026-07-28 17:41:48 +0800

    keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -2,9 +2,28 @@ local cmd = require("cal.mapping").cmd
 local map = require("cal.mapping").map
 
 map("t", "<ESC>", [[<C-\><C-N>]])
+map("v", "<Enter>", '"+y')
+map("n", "<M-u>", "zt")
+map("n", "<M-w>", cmd("bd"))
+map("n", "<M-n>", cmd("enew"))
+
+-- pane navigation
+map("n", "<M-h>", cmd("wincmd h"))
+map("n", "<M-j>", cmd("wincmd j"))
+map("n", "<M-k>", cmd("wincmd k"))
+map("n", "<M-l>", cmd("wincmd l"))
+
+-- bufs + tabs navigation
+map("n", "<M-I>", cmd("tabprevious"))
+map("n", "<M-O>", cmd("tabnext"))
 map("n", "<M-i>", cmd("bprevious"))
 map("n", "<M-o>", cmd("bnext"))
-map("v", "<Enter>", '"+y')
+-- bufs + tabs navigation
+map("n", "<M-H>", cmd("tabprevious"))
+map("n", "<M-J>", cmd("bnext"))
+map("n", "<M-K>", cmd("bprevious"))
+map("n", "<M-L>", cmd("tabnext"))
+-- END bufs + tabs navigation
 
 map("n", "<Tab>i", cmd("tabprevious"))
 map("n", "<Tab>o", cmd("tabnext"))
